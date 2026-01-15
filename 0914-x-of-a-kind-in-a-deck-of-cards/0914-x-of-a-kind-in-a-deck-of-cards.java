c972a06b46fea6aaa0958461c311a0a45eac3bb7
Aneesh Raj S <aneesh.rajx@gmail.com>
2026-01-16 00:05:04 +0530

    Time: 11 ms (65.51%), Space: 47.3 MB (43.12%) - LeetHub

---
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int num:deck){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        int gcd=0;
+        for(int count:map.values()){
+            gcd=gcd(gcd,count);
+        }
+        return gcd>=2;
+
+
+
+        
+    }
+    static int gcd(int a,int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+}
