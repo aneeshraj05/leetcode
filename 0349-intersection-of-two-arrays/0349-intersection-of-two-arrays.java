4cfe4603cf93990e66cfdb44cf594dd8d1a85a0c
Aneesh Raj S <aneesh.rajx@gmail.com>
2026-06-16 10:40:35 +0530

    Time: 0 ms (100%), Space: 44.6 MB (89.03%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,25 +1,48 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int nums:nums1)
-        {
-            map.put(nums,0);
-        }
-      List<Integer> list=new ArrayList<>();
-      for(int num:nums2){
-        if(map.containsKey(num)){
-            list.add(num);
-            map.remove(num);
-        }
-      }
-int size=list.size();
-int arr[]=new int[size];
-for(int i=0;i<arr.length;i++)
-{
-    arr[i]=list.get(i);
+//         Map<Integer,Integer> map=new HashMap<>();
+//         for(int nums:nums1)
+//         {
+//             map.put(nums,0);
+//         }
+//       List<Integer> list=new ArrayList<>();
+//       for(int num:nums2){
+//         if(map.containsKey(num)){
+//             list.add(num);
+//             map.remove(num);
+//         }
+//       }
+// int size=list.size();
+// int arr[]=new int[size];
+// for(int i=0;i<arr.length;i++)
+// {
+//     arr[i]=list.get(i);
+// }
+
+// return arr;
+
+
+
+boolean arr[]=new boolean[1001];
+for(int num:nums1){
+    arr[num]=true;
 }
 
-return arr;
+int count=0;
+int man[]=new int[1001];
+for(int num:nums2){
+    if(arr[num]){
+        man[count]=num;
+        count++;
+        arr[num]=false;
 
+    }
+}
+int arr1[]=new int[count];
+for(int i=0;i<arr1.length;i++){
+    arr1[i]=man[i];
+}
+
+return arr1;
     }
 }
