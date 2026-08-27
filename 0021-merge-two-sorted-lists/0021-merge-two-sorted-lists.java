bb88617e0be5bbce4ac8a19c80529771fc6b9c53
Aneesh Raj S <aneesh.rajx@gmail.com>
2026-08-27 09:42:53 +0530

    Time: 0 ms (100%), Space: 44.4 MB (38.74%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -15,22 +15,30 @@ class Solution {
         while(list1!=null && list2!=null){
             if(list1.val>=list2.val){
                 temp.next=list2;
-                list2=list2.next;
-            
+list2=list2.next;
             }
             else{
                 temp.next=list1;
                 list1=list1.next;
+                
             }
             temp=temp.next;
+           
+
+
 
         }
-        if(list1==null){
-            temp.next=list2;
-        }
-        else{
-            temp.next=list1;
-        }
+
+         if(list2==null){
+                temp.next=list1;
+
+
+            }
+            else{
+                temp.next=list2;
+            }
+
+
         return dummy.next;
         
     }
